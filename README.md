85b4c3e185fb5a74926a4a1b9516997c4a3fdffa
yk2021yeah <ykitani2018@yahoo.co.jp>
2022-04-04 02:10:39 +0900

    change
    brush up

---
+++ README.md
@@ -0,0 +1,13 @@
+# HappyProject backend
+
+## Concept
+
+This project aims to create an project management web application which is simple and everyone can use it as a single source of project related items. From task scheduling to issue tracking, every item will need user-defined level of approval. To eliminate over-committed milestones, unhandled development schedule, etc... Motivated to achieve elimination of any spread sheet application to manage projects. Bunch of tabbed sheets, uncotrolled copies spread out to unknown folks.
+
+## This backend
+
+Utilizing MongoDB Atlas free account as database and the backend web server API is written in Rust. As a server, axum was picked up since it's under Tokio. At first, preparing CRUD API of project data (note: current code is just at starting point and using non-related sample data provided by MongoDB free account.)
+
+## Frontend
+
+will be created as Webassembly by yew stack.
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 use axum::{
-    routing::{get, post},
     http::StatusCode,
     response::IntoResponse,
+    routing::{get, post},
     Json, Router,
 };
+use futures::stream::TryStreamExt;
+use mongodb::bson::{doc, oid::ObjectId, Document};
+use mongodb::options::{ClientOptions, FindOptions};
+use mongodb::Client;
 use serde::{Deserialize, Serialize};
+use std::env;
 use std::net::SocketAddr;
+use std::result::Result;
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
-    // build our application with a route
     let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root))
-        // `POST /users` goes to `create_user`
+        .route("/check", get(check))
+        .route("/restaurants", get(restaurants))
+        .route("/find_restaurants", get(get_restaurants))
         .route("/users", post(create_user));
 
     // run our app with hyper
@@ -29,9 +34,61 @@ async fn main() {
         .unwrap();
 }
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn check() -> &'static str {
+    "It's working."
+}
+
+async fn restaurants() -> Result<impl IntoResponse, impl IntoResponse> {
+    let connection = env::var("MONGODB_CONNECTION_STRING");
+    println!("Connection String {}", connection.unwrap());
+    let mut client_options = ClientOptions::parse(
+        env::var("MONGODB_CONNECTION_STRING").unwrap(),
+    )
+    .await
+    .unwrap();
+    client_options.app_name = Some("HappyProject".to_string());
+    let client = Client::with_options(client_options).unwrap();
+
+    let db = client.default_database().unwrap();
+    let collection = db.collection::<Document>("restaurants");
+
+    let docs = vec![
+        doc! { "borough": "Brooklyn", "cuisine": "American", "name": "YesNo Restaurant" },
+        doc! { "title": "Animal Farm", "author": "George Orwell" },
+        doc! { "title": "The Great Gatsby", "author": "F. Scott Fitzgerald" },
+    ];
+
+    let inserted = collection.insert_many(&docs, None).await;
+    match inserted {
+        Ok(_r) => Ok((StatusCode::CREATED, Json(docs))),
+        Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, Json(e.to_string()))),
+    }
+}
+
+async fn get_restaurants() -> impl IntoResponse {
+    let mut client_options = ClientOptions::parse(
+        env::var("MONGODB_CONNECTION_STRING").unwrap()
+    )
+    .await
+    .unwrap();
+    client_options.app_name = Some("HappyProject".to_string());
+    let client = Client::with_options(client_options).unwrap();
+
+    let db = client.default_database().unwrap();
+    let collection = db.collection::<Restaurant>("restaurants");
+
+    let filter = doc! { "cuisine": "American", "name": "YesNo Restaurant" };
+    let find_options = FindOptions::builder().sort(doc! { "_id": -1 }).build();
+    let mut cursor = collection
+        .find(filter, find_options)
+        .await
+        .unwrap();
+
+    let mut vec: Vec<Restaurant> = Vec::new();
+    while let Some(restaurant) = cursor.try_next().await.unwrap() {
+        vec.push(restaurant);
+    }
+    (StatusCode::OK, Json(vec))
 }
 
 async fn create_user(
@@ -50,6 +107,12 @@ async fn create_user(
     (StatusCode::CREATED, Json(user))
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Restaurant {
+    _id: ObjectId,
+    name: String,
+    cuisine: String,
+}
 // the input to our `create_user` handler
 #[derive(Deserialize)]
 struct CreateUser {
